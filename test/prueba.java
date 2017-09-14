c7fd83b2bd00278c6c34db47b23539ae7bfa49bf
nomad4real <bcdeuce@gmail.com>
2017-09-14 08:36:35 -0300

    Calculas cuota no funcionam, se me olvido si la cuota se ajusta a quexDD

---
+++ test/prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nomad
+ */
+
+import metodos.consultor;
+
+public class prueba {
+    public static void main(String[] args) {
+        consultor x = new consultor();
+        x.calcularUltimaCuota(87, 3);
+//        x.generar_listado_cuotas(345, 10);
+    }
+}
